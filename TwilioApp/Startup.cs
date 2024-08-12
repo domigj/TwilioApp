9a91b1509a6f4d1993bf4a11637c17af12b9faf2
Domigj <domigojime@gmail.com>
2024-08-12 13:30:50 -0600

    Add project files.

---
+++ TwilioApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TwilioApp.Startup))]
+namespace TwilioApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
